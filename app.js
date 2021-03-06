d6a9c6c4a9914cfefe05f80c81392594579b4e11
Skoraskrzy <krzysztof@elmanserwis.pl>
2021-03-06 09:13:19 +0100

    przedoddaniem

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const app = express();
 app.listen(3000, () => {
     console.log('serwer działa');
 });
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 app.use(express.static(
     path.join( 'public'),
 ));
--- routes/program.js
+++ routes/program.js
@@ -1,64 +1,73 @@
 const fs = require('fs');
 const nodemailer = require('nodemailer');
+const path = require('path');
+var glob = require("glob");
 
-function keyence(app){
-    let imgcounter = 0;
-    let imgNg = [];
-
-    console.log('plik dodany');
+function keyence(app){              //główny moduł
+    let imgcounter;
+    var imgpatharray = [];    
+       
     //funkcja sprawdzająca czy są fotki z błędami
-    function checkfolder(){
+    function checkfolder(){   
+
+        glob("../../../../srv/ftp/**/*.bmp", function (er, files) {
+            imgpatharray = files;
+      })            //glob daje tablicę ze ścieżkami wszystkich plików
+       
     fs.readdir('/srv/ftp', function(err, files) {     //sprawdzić co jest w files
         if (err) {
            // some sort of error
         } else {
            if (!files.length) {         //brak fotek
-                imgcounter = 0;
-           } else {
-                imgcounter = files.length;      //fotki są ilość i tablica
-             
-                imgNg = files;
-                console.log(imgcounter);
-                console.log(imgNg);
-            }
-        }
-    });
+               imgcounter = 0;
+               console.log(imgcounter);
+                            
+           } else { 
 
-   };
-   setInterval(checkfolder, 10000);
-   //obsługa emaila
-   function sendmail(tytul, tresc, dokogo){
-     var transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-            user: 'krzysiek.skoras@gmail.com',
-            pass: 'skoras1029'
-            }
-        });
-        
-        var mailOptions = {
-            from: 'krzysiek.skoras@gmail.com',
-            to: dokogo,
-            subject: tytul,
-            text: tresc
-        };
-        
-        transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-            console.log(error);
-            } else {
-            console.log('Email sent: ' + info.response);
-            }
-        });
-    };
+                imgcounter = files.length;      //ile zdjęć                    
+                //sending email
+                    var transporter = nodemailer.createTransport({
+                        service: 'gmail',
+                        auth: {
+                        user: 'krzysiek.skoras@gmail.com',
+                        pass: 'skoras1029'
+                        }
+                    });
+                    
+                    var mailOptions = {
+                        from: 'krzysiek.skoras@gmail.com',
+                        to: 'jaroslaw.krol@jti.com',
+                        subject: 'Wiadomość z bufora MP09',
+                        text: 'Wykryto uszkodzenia  na pasie bufora.'
+                    };
+                    
+                    transporter.sendMail(mailOptions, function(error, info){
+                        if (error) {
+                        console.log(error);
+                        } else {
+                        console.log('Email sent: ' + info.response);
+                        }
+                    
+                });                         
+            }           // koniec else obsługa błędu
+        }
+    });              //koniec fs
+
+   };               //koniec pętli
+   var myinterval = setInterval(checkfolder, 5000);  
     
     app.get('/checkresult', (req, res) => {
         res.json({
-            ng: imgcounter,
-            ngimg: imgNg,
-        });
+            ngvalue: imgcounter,           //ilosc zdjec
+            
+            });        
     });
 
+     app.get('/fotka', (req, res) => {
+        const filename = path.join(__dirname,  imgpatharray[0]);        //pierwsza fotka
+        res.sendFile(filename);
+    }); 
+    
 }
 
 module.exports = keyence;
